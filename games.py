7cc2e0c0ab55b31c9f4ac7e0ad89fab76a83c922
Jude Shamsi <judeshamsi@Judes-MacBook-Air.local>
2020-05-04 22:45:07 -0700

    added sprite player and background

--- games.py
+++ games.py
@@ -1,11 +1,13 @@
 import pygame
 import random
 import sys
+from os import path
+
+img_dir = path.join(path.dirname(__file__), 'img')
 
 
-
-WIDTH = 800
-HEIGHT = 600
+WIDTH = 600
+HEIGHT = 800
 PLAYER_COLOUR = (255, 0, 0)
 ENEMY_COLOUR = (0,255,0)
 BACKGROUND_COLOUR = (0,0,0)
@@ -19,11 +21,11 @@ pygame.display.set_caption("Toilet Paper Frenzy")
 clock = pygame.time.Clock()
 
 player_size = 50
-player_speed = 40
+player_speed = 5
 player_pos = [WIDTH/2, HEIGHT-2*player_size] # x coordinate for starting rectangle
 
 
-enemy_speed = 10
+enemy_speed = 0
 enemy_size = 50
 enemy_pos = [random.randint(0, WIDTH-enemy_size), 0] #use random.randomint to randomly assign a position for the enemy
 enemy_list = [enemy_pos]
@@ -34,23 +36,80 @@ gameover = False
 
 font = pygame.font.SysFont("monospace", 35)
 
-class ToiletPaper(pygame.sprite.Sprite):
+#Load game graphics
+
+background = pygame.image.load(path.join(img_dir, "bg_02_v.png")).convert()
+background_rect = background.get_rect()
+player_img = pygame.image.load(path.join(img_dir, "penguin_16x16.jpg"))
+player_rect = player_img.get_rect()
+player_rect.center = WIDTH / 2, HEIGHT / 2
+
+class Penguin(pygame.sprite.Sprite):
 	def __init__(self):
 		pygame.sprite.Sprite.__init__(self)
-		self.image =  pygame.Surface((50, 40))
-		self.image.fill((0, 255, 0))
+		self.image =  player_img
 		self.rect = self.image.get_rect()
 		self.rect.centerx = WIDTH / 2
 		self.rect.centery = HEIGHT - 10
 		self.speedx = 0
+		self.speedy = 0
 
 	def update(self):
+		
+		x = self.rect.centerx
+		y = self.rect.centery
+
+		keystate = pygame.key.get_pressed()
+
+
+
+			# prevent player from going off boundary of screen + key functionality
+		if keystate[pygame.K_LEFT]:
+			if x - player_size < 0:
+				x = 0
+			else:
+				self.speedx = -player_speed					
+		if keystate[pygame.K_RIGHT]:
+			if x + player_size >= WIDTH:
+				x = WIDTH - player_size
+			else:
+				self.speedx = player_speed
+		if keystate[pygame.K_UP]:
+			if y - player_size < 0:
+				y = 0 
+			else:
+				self.speedy = -player_speed
+		if keystate[pygame.K_DOWN]:
+			if y + player_size >= HEIGHT:
+				y = HEIGHT - player_size
+			else:
+				self.speedy = player_speed
+
 		self.rect.x += self.speedx
+		self.rect.y += self.speedx
+		
+		player_pos = [x, y]
+
+			# 	self.speedx = 0
+	# 	keystate = pygame.key.get_pressed()
+	# 	if keystate[pygame.K_LEFT]:
+	# 		self.speedx -= player_speed
+	# 	if keystate[pygame.K_RIGHT]:
+	# 		self.speedx += player_speed
+	# 	if keystate[pygame.K_UP]:
+	# 		self.speedy -= player_speed
+	# 	if keystate[pygame.K_DOWN]:
+	# 		self.speedy += player_speed
+
+	# 	self.rect.x += self.speedx
+	# 	#self.rect.y += self.speedy
+
+	# 	player_pos = [self.rect.x, self.rect.y]
+
 
 all_sprites = pygame.sprite.Group()
-toilet_paper = ToiletPaper()
-all_sprites.add(toilet_paper)
-
+penguin = Penguin()
+all_sprites.add(penguin)
 
 
 def show_game_over_screen():
@@ -62,12 +121,12 @@ def show_game_over_screen():
 def set_level(score, enemy_speed):
 
 	if score < 20:
-		enemy_speed = 1
+		enemy_speed = 2
 
 	elif score < 40:
-		enemy_speed = 2
-	else: 
 		enemy_speed = 3
+	else: 
+		enemy_speed = 2
 	
 	return enemy_speed
 
@@ -76,7 +135,7 @@ def drop_enemies(enemy_list):
 	delay = random.random() #generates number from 0 to 1
 
 	if len(enemy_list) < 10 and delay < 0.1: #keep adding enemies until 10 are on screen
-		x_pos = random.randint(0, WIDTH-enemy_size) #generate new enemy position on top of screen
+		x_pos = random.randrange(0, WIDTH-enemy_size) #generate new enemy position on top of screen
 		y_pos = 0
 		enemy_list.append([x_pos, y_pos])
 
@@ -90,7 +149,7 @@ def update_enemy_pos(enemy_list, score):
 	
 	for index, enemy_pos in enumerate(enemy_list):
 		if enemy_pos[1] >= 0 and enemy_pos[1] < HEIGHT: #enemy is on screen
-			enemy_pos[1] += enemy_speed #drop enemy by speed
+				enemy_pos[1] += enemy_speed #drop enemy by speed
 		else:
 			enemy_list.pop(index)
 			score += 1
@@ -122,7 +181,7 @@ def detect_collision(player_pos, enemy_pos):
 
 
 #will loop while game is in use to run all the code below
-while running:
+while not gameover:
 
 	#loop will track all key events
 	for event in pygame.event.get():
@@ -130,45 +189,51 @@ while running:
 		if event.type == pygame.QUIT:
 			sys.exit()
 
-		if event.type == pygame.KEYDOWN:
 
-			x = player_pos[0]
-			y = player_pos[1]
 
-			if event.key == pygame.K_LEFT:
-				if x - player_size < 0:
-					x = 0
-				else:
-					x -= player_speed					
-			if event.key == pygame.K_RIGHT:
-				if x + player_size >= WIDTH:
-					x = WIDTH - player_size
-				else:
-					x += player_speed
-			if event.key == pygame.K_UP:
-				if y - player_size < 0:
-					y = 0 
-				else:
-					y -= player_speed
-			if event.key == pygame.K_DOWN:
-				if y + player_size >= HEIGHT:
-					y = HEIGHT - player_size
-				else:
-					y += player_speed
+		#Update
+		all_sprites.update()
+
+		#if event.type == pygame.KEYDOWN:
+
+			#x = player_pos[0]
+			#y = player_pos[1]
+
+			# x = penguin.centerx
+			# y = penguin.centery
+
+
+			# # prevent player from going off boundary of screen + key functionality
+			# if event.key == pygame.K_LEFT:
+			# 	if x - player_size < 0:
+			# 		x = 0
+			# 	else:
+			# 		x -= player_speed					
+			# if event.key == pygame.K_RIGHT:
+			# 	if x + player_size >= WIDTH:
+			# 		x = WIDTH - player_size
+			# 	else:
+			# 		x += player_speed
+			# if event.key == pygame.K_UP:
+			# 	if y - player_size < 0:
+			# 		y = 0 
+			# 	else:
+			# 		y -= player_speed
+			# if event.key == pygame.K_DOWN:
+			# 	if y + player_size >= HEIGHT:
+			# 		y = HEIGHT - player_size
+			# 	else:
+			# 		y += player_speed
 			
 
-			player_pos = [x,y]
+			#player_pos = [x, y]
 
-
-	# all_sprites.update()
 
 	# this resets the screen background each time the loop runs, allowing a new rectangle to be drawn
 	screen.fill(BACKGROUND_COLOUR)
-	# all_sprites.draw(screen)
-	# pygame.display.flip()
+	screen.blit(background, background_rect)
 
-
-
+	all_sprites.draw(screen)
 	drop_enemies(enemy_list)
 	score = update_enemy_pos(enemy_list, score) #score is integer ie. immutable value so must pass it to change
 	enemy_speed = set_level(score, enemy_speed)
@@ -179,20 +244,80 @@ while running:
 
 	if collision_check(enemy_list, player_pos):
 		gameover = True
-		running = False
 		break
 		
 
 	draw_enemies(enemy_list)
 
-	pygame.draw.rect(screen, PLAYER_COLOUR, (player_pos[0], player_pos[1], player_size, player_size))
+	#screen.blit(player_img, player_rect)
+	#pygame.draw.rect(screen, PLAYER_COLOUR, player_rect, 1)
+	#pygame.draw.rect(screen, ENEMY_COLOUR, (player_rect.left, player_rect.top), 1)
 
 	#update screen every iteration so you can see the visualizations
 
 	clock.tick(FPS)
 
 
-	pygame.display.update()
+	pygame.display.flip()
 
 
 
+
+
+# class Penguin(pygame.sprite.Sprite):
+# 	def __init__(self):
+# 		pygame.sprite.Sprite.__init__(self)
+# 		self.image =  player_img
+# 		self.image.fill((0, 255, 0))
+# 		self.rect = self.image.get_rect()
+# 		self.rect.centerx = WIDTH / 2
+# 		self.rect.centery = HEIGHT - 10
+# 		self.speedx = 0
+
+# 	def update(self):
+# 		self.rect.x += self.speedx
+
+# all_sprites = pygame.sprite.Group()
+# penguin = Penguin()
+# all_sprites.add(penguin)
+
+# class Mob(pygame.sprite.Sprite):
+# 	def __init__(self):
+# 		pygame.sprite.Sprite.__init__(self)
+# 		self.image = pygame.Surface((30, 40))
+# 		self.image.fill((255,0,0))
+# 		self.rect = self.image.get_rect()
+# 		self.rect.x = random.randrange(WIDTH - self.rect.width)
+# 		self.rect.y = random.randrange(-100, -40)
+# 		self.speedy = random.randrange(1, 8)
+# 		self.speedx = random.randrange(-3, 3)
+
+# 	def update(self):
+# 		self.rect.x += self.speedx
+# 		self.rect.y += speedy
+# 		if self.rect.top > HEIGHT + 10 or self.rect.left < -25 or self.rect.right > WIDTH + 20:
+# 			self.rect.x = random.randrange(WIDTH - self.rect.width)
+# 			self.rect.y = random.randrange(-100, -40)
+# 			self.speedy = random.randrange(1, 8)
+
+
+		#if event.key == pygame.K_LEFT:
+		# 		if x - player_size < 0:
+		# 			x = 0
+		# 		else:
+		# 			x -= player_speed					
+		# 	if event.key == pygame.K_RIGHT:
+		# 		if x + player_size >= WIDTH:
+		# 			x = WIDTH - player_size
+		# 		else:
+		# 			x += player_speed
+		# 	if event.key == pygame.K_UP:
+		# 		if y - player_size < 0:
+		# 			y = 0 
+		# 		else:
+		# 			y -= player_speed
+		# 	if event.key == pygame.K_DOWN:
+		# 		if y + player_size >= HEIGHT:
+		# 			y = HEIGHT - player_size
+		# 		else:
+		# 			y += player_speed
